c72f2fa74377719c14b11f386d8f1032f36fd9b4
parvesh <polidahiya830@gmail.com>
2025-04-02 18:50:57 +0530

    .

--- app/[location]/layout.jsx
+++ app/[location]/layout.jsx
@@ -3,17 +3,17 @@ import { headers } from "next/headers";
 import { redirect } from "next/navigation";
 
 function layout({ children }) {
-  const headersList = headers();
-  const fullUrl = headersList.get("referer") || headersList.get("host");
-  const pathname = new URL(fullUrl || `http://${headersList.get("host")}`)
-    .pathname;
+  //   const headersList = headers();
+  //   const fullUrl = headersList.get("referer") || headersList.get("host");
+  //   const pathname = new URL(fullUrl || `http://${headersList.get("host")}`)
+  //     .pathname;
 
-  const splitpath = pathname.split("/");
-  if (splitpath[1] == "null") {
-    splitpath[1] = "Delhi";
-    const newpath = splitpath.join("/");
-    redirect(newpath);
-  }
+  //   const splitpath = pathname.split("/");
+  //   if (splitpath[1] == "null") {
+  //     splitpath[1] = "Delhi";
+  //     const newpath = splitpath.join("/");
+  //     redirect(newpath);
+  //   }
   return <>{children}</>;
 }
 
