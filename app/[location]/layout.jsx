01be1dcb70ab7b68adefb17da07460eba826ec57
parvesh <polidahiya830@gmail.com>
2025-04-02 18:36:29 +0530

    .

---
+++ app/[location]/layout.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
+
+function layout({ children }) {
+  const headersList = headers();
+  const fullUrl = headersList.get("referer") || headersList.get("host");
+  const pathname = new URL(fullUrl || `http://${headersList.get("host")}`)
+    .pathname;
+
+  const splitpath = pathname.split("/");
+  if (splitpath[1] == "null") {
+    splitpath[1] = "Delhi";
+    const newpath = splitpath.join("/");
+    redirect(newpath);
+  }
+  return <>{children}</>;
+}
+
+export default layout;
